71d02c1e56f03be5561184e78fa974f18acfce71
m-ghozi <ghozi286@gmail.com>
2024-02-10 00:36:33 +0700

    update

---
+++ 15-returnNegative.js
@@ -0,0 +1,20 @@
+// Return Negative
+// https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+
+// function makeNegative(num) {
+//   if (num > 0) return num - num * 2
+//   else return num + num * -2
+// }
+
+// function makeNegative(num) {
+//   return -Math.abs(num)
+// }
+
+// function makeNegative(num) {
+//   return num < 0 ? num : -num;
+// }
+
+makeNegative = n => -Math.abs(n)
+
+console.log(makeNegative(10))
---
+++ 16-countByX.js
@@ -0,0 +1,16 @@
+// Count by X
+// https://www.codewars.com/kata/5513795bd3fafb56c200049e
+
+// function countBy(x, n) {
+//   let z = [];
+//   for (let i = 1;i <= n;i++) {
+//     z.push(i*x)
+//   }
+//   return z;
+// }
+
+const countBy = (x,n) => [...Array(n)].map((_,i) => (i + 1) * x);
+
+console.log(countBy(1,10));
+console.log(countBy(2,5));
+console.log(countBy(5,2));
