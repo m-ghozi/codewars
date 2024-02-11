a4b5ab5f841472e9dd4834062c0d06978c6f1c4c
m-ghozi <ghozi286@gmail.com>
2024-02-12 00:17:21 +0700

    update 12-02-24

---
+++ 26-vowelCount.js
@@ -0,0 +1,14 @@
+// Vowel Count
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+const getCount = str => {
+    let output = str.match(/a|i|u|e|o/gi);
+    return output != null ? output.length : 0;
+}
+
+// function getCount(str) {
+//   return (str.match(/[aeiou]/ig)||[]).length;
+// }
+
+console.log(getCount("abracadabra"));
+console.log(getCount("mypyx"));
