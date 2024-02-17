397e15b9f3800bd711df3520102f729c0645536e
m-ghozi <ghozi286@gmail.com>
2024-02-17 19:33:25 +0700

    17-02-24

---
+++ 39-keepHydrated.js
@@ -0,0 +1,11 @@
+// Keep Hydrated!
+// https://www.codewars.com/kata/582cb0224e56e068d800003c
+
+const litres = time => Math.floor(time * 0.5);
+
+function litres(time) {
+  return parseInt(time/2);
+}
+
+console.log(litres(1.4))
+console.log(litres(2))
