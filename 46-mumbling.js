a25505b2ab9b7c56da3a7d192bbdf939831862d4
m-ghozi <ghozi286@gmail.com>
2024-02-23 23:52:06 +0700

    23-02-24

--- 46-mumbling.js
+++ 46-mumbling.js
@@ -1,23 +1,23 @@
 // Mumbling
 // https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
 
-// function accum(s) {
-//   let splited = s.toLowerCase().split('')
-//   let result = []
-//   for (let i = 0; i < splited.length; i++){
-//     for (let x = 0; x <= i; x++){
-//       if (x == 0){
-//         result.push(splited[i].toUpperCase())
-//       }
-//       else {
-//         result.push(splited[i])
-//       }
-//     }
-//     result.push('-')
-//   }
-// 	result.pop()
-// 	return result.join('')
-// }
+function accum(s) {
+  let splited = s.toLowerCase().split('')
+  let result = []
+  for (let i = 0; i < splited.length; i++){
+    for (let x = 0; x <= i; x++){
+      if (x == 0){
+        result.push(splited[i].toUpperCase())
+      }
+      else {
+        result.push(splited[i])
+      }
+    }
+    result.push('-')
+  }
+	result.pop()
+	return result.join('')
+}
 
 const accum = (s) =>
   s
