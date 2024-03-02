f1c152e2073818c1a76f73ac74dd787133504ee3
m-ghozi <ghozi286@gmail.com>
2024-03-02 14:02:30 +0700

    02-03-24

---
+++ 52-areaOrPerimeter.js
@@ -0,0 +1,15 @@
+// Area or Perimeter
+// https://www.codewars.com/kata/5ab6538b379d20ad880000ab
+
+const areaOrPerimeter = function(l , w) {
+  if (l === w){
+    return l * w
+  } else {
+    return 2 * ( l + w)
+  }
+};
+
+const areaOrPerimeter = (l,w) => l === w ? l * w : 2 * (l + w);
+
+console.log(areaOrPerimeter(3,  3));
+console.log(areaOrPerimeter(6,  10));
